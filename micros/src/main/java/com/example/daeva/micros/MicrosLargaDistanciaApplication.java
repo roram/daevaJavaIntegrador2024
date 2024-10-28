8390997cf149e04455541c34543f2d46f7b7c534
roram <ces.ram.franco@gmail.com>
2024-10-28 20:09:09 -0300

    [Cesar_Ramirez] - Inicialización de proyecto
     Agrego las librerías básicas para desarrollar le proyecto integrador.
    
     Changes to be committed:
    	new file:   micros/.gitattributes
    	new file:   micros/.gitignore
    	new file:   micros/.mvn/wrapper/maven-wrapper.properties
    	new file:   micros/mvnw
    	new file:   micros/mvnw.cmd
    	new file:   micros/pom.xml
    	new file:   micros/src/main/java/com/example/daeva/micros/MicrosLargaDistanciaApplication.java
    	new file:   micros/src/main/resources/application.properties
    	new file:   micros/src/test/java/com/example/daeva/micros/MicrosLargaDistanciaApplicationTests.java

---
+++ micros/src/main/java/com/example/daeva/micros/MicrosLargaDistanciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.daeva.micros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosLargaDistanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosLargaDistanciaApplication.class, args);
+	}
+
+}
---
+++ micros/src/test/java/com/example/daeva/micros/MicrosLargaDistanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daeva.micros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosLargaDistanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
