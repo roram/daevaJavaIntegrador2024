ce78e529f94720da6461571eeb26e384e7da0870
lr91723 <luis_roggiano@itrsa.com.ar>
2024-10-31 18:36:15 -0300

    update clase ciudades

--- entrega/src/main/java/ar/daeva/utn/entrega/models/entities/Ciudades.java
+++ entrega/src/main/java/ar/daeva/utn/entrega/models/entities/Ciudades.java
@@ -1,4 +1,21 @@
 package ar.daeva.utn.entrega.models.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="ciudades")
+
 public class Ciudades {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private Integer kms;
+
 }
--- entrega/src/main/java/ar/daeva/utn/entrega/models/entities/Micro.java
+++ entrega/src/main/java/ar/daeva/utn/entrega/models/entities/Micro.java
@@ -9,7 +9,8 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name="micro")
-
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@DiscriminatorColumn(name="tipo_producto")
 public class Micro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
